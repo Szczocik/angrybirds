21aaf0f3c679c79f0fc8fbdae9e369c6b6c31d7e
Szczocik <popa.mariusz@gmail.com>
2023-08-28 16:51:27 +0200

    Obrót w kierunku punktu

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,6 +20,7 @@ public class Player : MonoBehaviour
         cam = Camera.main;  
         lineRenderer.SetPosition(0, pivot.position);
         lineRenderer.SetPosition(1, transform.position);
+        RotatePlayer();
     }
 
     void Update()
@@ -32,10 +34,19 @@ public class Player : MonoBehaviour
         tempPos.z = 0f; // camera ustawiona jest standardowo na z = -10 i dlatego musimy to zmieniæ na 0
         transform.position = tempPos;
         lineRenderer.SetPosition(1, tempPos);
+        RotatePlayer();
     }
 
     private void OnMouseUp()
     {
         lineRenderer.SetPosition(1, pivot.position);
+    }
+
+    void RotatePlayer()
+    {
+        Vector3 diff = transform.position - pivot.position;
+        diff.Normalize();
+        float rotZ = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f,0f, rotZ + 90f);
     }
 }
